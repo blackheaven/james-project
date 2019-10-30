12a8d8944991eb6f6c9044ba0d4c945c1643f9c8
Gautier DI FOLCO <gdifolco@linagora.com>
2019-10-30 15:19:16 +0100

    fixup! JAMES-2946 Return InvalidArgument when a MailRepository is tried to be created with an unsupported protocol

--- server/protocols/webadmin/webadmin-mailrepository/src/main/java/org/apache/james/webadmin/routes/MailRepositoriesRoutes.java
+++ server/protocols/webadmin/webadmin-mailrepository/src/main/java/org/apache/james/webadmin/routes/MailRepositoriesRoutes.java
@@ -151,12 +151,19 @@ public class MailRepositoriesRoutes implements Routes {
             try {
                 repositoryStoreService.createMailRepository(path, protocol);
                 return Responses.returnNoContent(response);
-            } catch (MailRepositoryStore.MailRepositoryStoreException e) {
+            } catch (MailRepositoryStore.UnsupportedRepositoryStoreException e) {
                 throw ErrorResponder.builder()
                     .statusCode(HttpStatus.BAD_REQUEST_400)
                     .type(ErrorType.INVALID_ARGUMENT)
                     .cause(e)
                     .message(String.format("'%s' is an unsupported protocol", protocol))
+                    .haltError();
+            } catch (MailRepositoryStore.MailRepositoryStoreException e) {
+                throw ErrorResponder.builder()
+                    .statusCode(HttpStatus.INTERNAL_SERVER_ERROR_500)
+                    .type(ErrorResponder.ErrorType.SERVER_ERROR)
+                    .cause(e)
+                    .message(String.format("Error while creating a mail repository with path '%s' and protocol '%s'", path.asString(), protocol))
                     .haltError();
             }
         }, jsonTransformer);
