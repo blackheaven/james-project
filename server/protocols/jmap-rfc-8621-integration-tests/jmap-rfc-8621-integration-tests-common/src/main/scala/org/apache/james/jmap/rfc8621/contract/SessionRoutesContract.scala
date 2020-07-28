6d7ea36301210913e1c59c6f4fc2fc1ba35e644a
Gautier DI FOLCO <gdifolco@linagora.com>
2020-07-28 13:21:01 +0200

    [Refactoring] Drop scala warnings

--- server/protocols/jmap-rfc-8621-integration-tests/jmap-rfc-8621-integration-tests-common/src/main/scala/org/apache/james/jmap/rfc8621/contract/SessionRoutesContract.scala
+++ server/protocols/jmap-rfc-8621-integration-tests/jmap-rfc-8621-integration-tests-common/src/main/scala/org/apache/james/jmap/rfc8621/contract/SessionRoutesContract.scala
@@ -112,7 +112,7 @@ trait SessionRoutesContract {
 
   @Test
   @Tag(CategoryTags.BASIC_FEATURE)
-  def getShouldReturnCorrectSession() {
+  def getShouldReturnCorrectSession(): Unit = {
     val sessionJson: String = `given`()
       .when()
         .header(ACCEPT.toString, ACCEPT_RFC8621_VERSION_HEADER)
